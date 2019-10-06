2924356fb5d726135bcedfc46458d13f2b86a424
Meg <northwoods2012@gmail.com>
2019-10-05 22:24:34 -0500

    css + js working! not perfect but better

--- blog/static/js/scripts.js
+++ blog/static/js/scripts.js
@@ -1,28 +1,29 @@
-$('#menu').css('left','-275px');
-$('#menu').hide();
-$('#other').css('margin-left','0px');
 
+$( document ).ready(function() {
+  console.log( 'ready!' );
+  $( "#menu " ).css('left','-275px');
+  $( "#other" ).css('margin-left','0px');
 
-$('#open-m').on('click', function(){
-  if(screen.width>601){
-    $('#menu').show();
-    $('#menu').css('left','0px');
-    $('#other').css('margin-left','275px');
-  }
-  else{
-    $('#menu').slideDown();
-    $('#other').slideUp();
-  }
-});
-
-$('#close-m').on('click', function(){
-  if(screen.width>601){
-    $('#menu').css('left','-275px');
-    $('#other').css('margin-left','0px');
-    $('#menu').hide();
-  }
-  else{
-    $('#menu').slideUp();
-    $('#other').slideDown();
-  }
+  $( "#open-m").on('click', function(){
+    if(screen.width>601){
+      $('#menu').show();
+      $('#menu').css('left','0px');
+      $('#other').css('margin-left','275px');
+    }
+    else{
+      $('#menu').slideDown();
+      $('#other').slideUp();
+    }
+  });
+  $('#close-m').on('click', function(){
+    if(screen.width>601){
+      $('#menu').css('left','-275px');
+      $('#other').css('margin-left','0px');
+      $('#menu').hide();
+    }
+    else{
+      $('#menu').slideUp();
+      $('#other').slideDown();
+    }
+  });
 });
